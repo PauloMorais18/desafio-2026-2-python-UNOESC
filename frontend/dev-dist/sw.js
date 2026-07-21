f7aab929d988c53e29b56c2b746dc64226ff62d8
Paulo Henrique de Morais <142455846+PauloMorais18@users.noreply.github.com>
2026-07-21 11:26:51 -0300

    alterar contexto somente em localhost

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,11 +78,12 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.ldt32gkc0do"
+    "revision": "0.gup8rlilf1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
-    allowlist: [/^\/$/]
+    allowlist: [/^\/$/],
+    denylist: [/^\/api\//]
   }));
 
 }));
